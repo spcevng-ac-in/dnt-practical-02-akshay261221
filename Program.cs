fbc9083cec3cd70a9fa1e4f5a7bd2469cb2a1098
AKSHAY <you@example.com>
2022-01-04 18:57:09 +0530

    submit practical-2

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace practical_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int km, wg, charge;
+            Console.WriteLine("Enter the value of km");
+            km = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the value of wg");
+            wg = int.Parse(Console.ReadLine());
+
+            if (km <= 100)
+            {
+                charge = 50*wg;
+            }
+            else if (km >= 100 && km <= 200)
+            {
+                charge = 65*wg;
+            }
+            else if (km >= 200 && km <= 300)
+            {
+                charge = 90*wg;
+            }
+            
+            else
+            {
+                charge= 120*wg;
+            }
+
+            Console.WriteLine("charges=" + charge);
+                Console.ReadLine();
+
+        }
+    }
+}
